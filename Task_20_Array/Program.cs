4a73a7d29dc68d4b30c9e371bad169ad46f9267c
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-26 21:10:59 +0300

    #1

---
+++ Task_20_Array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = { { 2, 3} , { 4, 5} };
+            int sumString = 0;
+            int columnProduct = 1;
+
+            Console.WriteLine("Исходная матрица: ");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j]);
+                    sumString += array[1, j];
+                }
+                Console.WriteLine("");
+                columnProduct *= array[i, 0];
+            }
+
+            Console.WriteLine($"\nСумма второй строки равна : {sumString}");
+            Console.WriteLine($"Произведение первого столбца равно: {columnProduct}\n");
+        }
+    }
+}
